85bf4b4f9443dc6c6e3ce3255eef3f38800bbd7b
Anthony <anthonykmbanks@gmail.com>
2021-03-15 10:21:35 -0400

    commit tree 6a578c91c058519026b7b27319629e67b4e38017

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 12
+const num4 = 3
+let mod = num3 / num4
+
+let max = Math.max(1,4,20)
